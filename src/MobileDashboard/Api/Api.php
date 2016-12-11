49bb0909c8803079bfafd4b20dde382083d90a3c
ondrejd <ondrejd@gmail.com>
2016-12-11 20:06:48 +0100

    Added basic classes of the API.

---
+++ src/MobileDashboard/Api/Api.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MobileDashboard JSON-REST API.
+ *
+ * @author Ondrej Donek, <ondrejd@gmail.com>
+ * @license GNU General Public License Version 3 http://www.gnu.org/licenses/gpl.txt
+ * @link https://github.com/MobileDashboard/server-php for canonical repository
+ */
+
+namespace MobileDashboard\Api;
+
+/**
+ * Main API class.
+ *
+ * @author Ondřej Doněk, <ondrejd@gmail.com>
+ */
+class Api
+{
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // ...
+    } // end __construct()
+
+    // ...
+
+} // End of Api
+
---
+++ src/MobileDashboard/Api/Item/Message.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MobileDashboard JSON-REST API.
+ *
+ * @author Ondrej Donek, <ondrejd@gmail.com>
+ * @license GNU General Public License Version 3 http://www.gnu.org/licenses/gpl.txt
+ * @link https://github.com/MobileDashboard/server-php for canonical repository
+ */
+
+namespace MobileDashboard\Api\Item;
+
+/**
+ * Class representing single dashboard message.
+ *
+ * @author Ondřej Doněk, <ondrejd@gmail.com>
+ */
+class Message
+{
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // ...
+    } // end __construct()
+
+    // ...
+
+} // End of Message
+
