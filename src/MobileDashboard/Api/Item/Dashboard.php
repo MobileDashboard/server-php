e8cd453c50db538bd5dd48c7bb1866a20b3819e8
ondrejd <ondrejd@gmail.com>
2016-12-12 18:21:57 +0100

    Finished Dashboard class.

--- src/MobileDashboard/Api/Item/Dashboard.php
+++ src/MobileDashboard/Api/Item/Dashboard.php
@@ -17,16 +17,115 @@ namespace MobileDashboard\Api\Item;
 class Dashboard
 {
     /**
+     * Unique numeric identifier of the dashboard.
+     * @var integer $id
+     */
+    protected $id;
+    
+    /**
+     * Unique numeric identifier of the dashboard's parent.
+     * @var integer $id
+     */
+    protected $parentId;
+    
+    /**
+     * Hash of the dashboard.
+     * @var string $hash
+     */
+    protected $hash;
+    
+    /**
+     * Displayed name of the dashboard.
+     * @var string $name
+     */
+    protected $name;
+
+    /**
      * Constructor
      *
+     * @param integer $id (Optional.) Id of the dashboard.
+     * @param integer $parentId (Optional.) Id of the dashboard's parent.
+     * @param string $hash (Optional.) Hash of the dashboard.
+     * @param string $name (Optional.) Name of the dashboard.
      * @return void
      */
-    public function __construct()
+    public function __construct($id = null, $parentId = null, $hash = null, $name = null)
     {
-        // ...
+        $this->setId($id);
+        $this->setParentId($parentId);
+        $this->setHash($hash);
+        $this->setName($name);
     } // end __construct()
 
-    // ...
+    /**
+     * @return integer ID of the dashboard.
+     */
+    public function getId() {
+        return $this->id;
+    } // end getId()
 
+    /**
+     * Set ID of the dashboard.
+     * @param integer $id
+     * @return void
+     */
+    public function setId($id) {
+        $this->id = (int) $id;
+    } // end setId($id)
+
+    /**
+     * @return integer ID of the dashboard's parent.
+     */
+    public function getParentId() {
+        return $this->parentId;
+    } // end getParentId()
+
+    /**
+     * Set ID of the dashboard's parent.
+     * @param integer $parentId
+     * @return void
+     */
+    public function setParentId($parentId) {
+        $this->parentId = (int) $parentId;
+    } // end setParentId($parentId)
+
+    /**
+     * @return string Hash of the dashboard.
+     */
+    public function getHash() {
+        return $this->hash;
+    } // end getHash()
+
+    /**
+     * Set hash of the dashboard.
+     * @param string $hash
+     * @return void 
+     */
+    public function setHash($hash) {
+        $this->hash = $hash;
+    } // end setHash($hash)
+
+    /**
+     * @return string Name of the dashboard.
+     */
+    public function getName() {
+        return $this->name;
+    } // end getName()
+
+    /**
+     * Set name of the dashboard.
+     * @param string $name
+     * @return void 
+     */
+    public function setName($name) {
+        $this->name = $name;
+    } // end setName($name)
+    
+    /**
+     * @return string
+     */
+    public function __toString() {
+        return $this->name;
+    } // end __toString()
 } // End of Dashboard
 
---
+++ test/src/MobileDashboard/Api/Item/DashboardTest.php
@@ -0,0 +1,69 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use MobileDashboard\Api\Item\Dashboard;
+
+class DashboardTest extends TestCase
+{
+    public function testConstruct()
+    {
+        $a = new Dashboard();
+        $this->assertEmpty($a->getId());
+        $this->assertEmpty($a->getParentId());
+        $this->assertEmpty($a->getHash());
+        $this->assertEmpty($a->getName());
+        $this->assertEmpty($a->__toString());
+    }
+    
+    public function testGetId()
+    {
+        $a = new Dashboard(11, 5, 'TEST0', 'Test Board');
+        $this->assertEquals(11, $a->getId());
+    }
+    
+    public function testSetId()
+    {
+        $a = new Dashboard();
+        $a->setId(11);
+        $this->assertEquals(11, $a->getId());
+    }
+    
+    public function testGetParentId()
+    {
+        $a = new Dashboard(11, 5, 'TEST0', 'Test Board');
+        $this->assertEquals(5, $a->getParentId());
+    }
+    
+    public function testSetParentId()
+    {
+        $a = new Dashboard();
+        $a->setParentId(5);
+        $this->assertEquals(5, $a->getParentId());
+    }
+    
+    public function testGetHash()
+    {
+        $a = new Dashboard(11, 5, 'TEST0', 'Test Board');
+        $this->assertEquals('TEST0', $a->getHash());
+    }
+    
+    public function testSetHash()
+    {
+        $a = new Dashboard();
+        $a->setHash('TEST0');
+        $this->assertEquals('TEST0', $a->getHash());
+    }
+    
+    public function testGetName()
+    {
+        $a = new Dashboard(11, 5, 'TEST0', 'Test Board');
+        $this->assertEquals('Test Board', $a->getName());
+        $this->assertEquals('Test Board', $a->__toString());
+    }
+    
+    public function testSetName()
+    {
+        $a = new Dashboard();
+        $a->setName('Test Board');
+        $this->assertEquals('Test Board', $a->getName());
+    }
+}
